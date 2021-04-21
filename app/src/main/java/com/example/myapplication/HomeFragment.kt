522503c14ee133434f88740e3fe7dc24daaf45be
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-04-21 09:25:10 +0200

    Home Fragment

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,35 +1,30 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class LoginFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.LogRegister).setOnClickListener {
-            findNavController().navigate(R.id.action_register)
-        }
-        view.findViewById<Button>(R.id.Login).setOnClickListener {
-            findNavController().navigate(R.id.login)
+        view.findViewById<Button>(R.id.Logout).setOnClickListener {
+            findNavController().navigate(R.id.logout)
         }
     }
 }
