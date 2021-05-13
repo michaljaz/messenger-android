cffd1275db309a2501d1d2e155a776435007a1c7
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-05-13 08:19:24 +0200

    basic notification (when registered)

--- app/src/main/java/com/example/myapplication/RegisterFragment.kt
+++ app/src/main/java/com/example/myapplication/RegisterFragment.kt
@@ -1,18 +1,31 @@
 package com.example.myapplication
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.fragment.findNavController
+import kotlin.with as with
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class RegisterFragment : Fragment() {
 
+    private val notificationManager: NotificationManager? = null
+    private val CHANNEL_ID="example_channel_id1"
+    private val notificationId=101
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -21,11 +34,34 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         view.findViewById<Button>(R.id.RegLogin).setOnClickListener {
             findNavController().navigate(R.id.action_login)
         }
+        val name="Notification title"
+        val descriptionText="xd"
+        val importance=NotificationManager.IMPORTANCE_DEFAULT
+        val channel=NotificationChannel(CHANNEL_ID,name,importance).apply {
+            description=descriptionText
+        }
+        val notificationManager=activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        view.findViewById<Button>(R.id.Register).setOnClickListener {
+            findNavController().navigate(R.id.action_login)
+            sendNotification()
+        }
+    }
+    private fun sendNotification() {
+        val builder=NotificationCompat.Builder(requireContext(),CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Example title")
+            .setContentText("Example description")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(requireContext())){
+            notify(notificationId,builder.build())
+        }
+
     }
 }
