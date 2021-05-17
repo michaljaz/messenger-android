2b1c135473622cf43ed1f47188750f68d0f47982
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-05-17 17:46:07 +0200

    socket.io server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log('here will be server')
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.send('socket.io is running!')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`)
+})
