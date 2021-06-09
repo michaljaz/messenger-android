471994472f93a46efa50daf66fd60210b39b5865
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-06-09 19:01:45 +0200

    firebase register

--- app/src/main/java/com/github/michaljaz/messenger/RegisterFragment.kt
+++ app/src/main/java/com/github/michaljaz/messenger/RegisterFragment.kt
@@ -7,22 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import io.socket.emitter.Emitter
 
 class RegisterFragment : Fragment() {
-    private var list: Emitter? = null
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        this.list=(activity as MainActivity).getSocket()?.once("signup_ok") {
-            showLog("sign up success")
-            try {
-                findNavController().navigate(R.id.action_login)
-            } catch (e: Exception){}
-        }
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
@@ -34,7 +30,23 @@ class RegisterFragment : Fragment() {
         view.findViewById<Button>(R.id.Register).setOnClickListener {
             val username=view.findViewById<TextInputLayout>(R.id.Email).editText?.text.toString()
             val password=view.findViewById<TextInputLayout>(R.id.Password).editText?.text.toString()
-            (activity as MainActivity).getSocket()?.emit("signup",username,password)
+            if(username != "" && password != ""){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(username,password)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            showLog("sign up success")
+                            val firebaseUser: FirebaseUser=task.result!!.user!!
+                            try {
+                                findNavController().navigate(R.id.action_login)
+                            } catch (e: Exception){}
+                        }else{
+                            Toast.makeText(context,task.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(context,"Empty email or password field!", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
     private fun showLog(message: String){
