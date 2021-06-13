3e0577124f57de549002c9b4a1859a63f5101703
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-06-13 20:20:15 +0200

    simple users listView

--- app/src/main/java/com/github/michaljaz/messenger/Users.kt
+++ app/src/main/java/com/github/michaljaz/messenger/Users.kt
@@ -1,7 +1,10 @@
 package com.github.michaljaz.messenger
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 
 class Users : Fragment() {
@@ -10,8 +13,16 @@ class Users : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view=inflater.inflate(R.layout.users, container, false)
         mactivity=(activity as MainActivity)
         mactivity.supportActionBar?.title = "Users"
-        return inflater.inflate(R.layout.users, container, false)
+        val list = view.findViewById<ListView>(R.id.list)
+        val users = arrayListOf ("Steve","Alex","Bob")
+        list.adapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            users
+        )
+        return view
     }
 }
