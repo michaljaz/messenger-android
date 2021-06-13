da2809d9675b0230cbd0278961561a01627c3043
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-06-13 20:38:12 +0200

    list real db users in users screen

--- app/src/main/java/com/github/michaljaz/messenger/Users.kt
+++ app/src/main/java/com/github/michaljaz/messenger/Users.kt
@@ -1,28 +1,45 @@
 package com.github.michaljaz.messenger
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import kotlin.reflect.typeOf
 
 class Users : Fragment() {
-    lateinit var mactivity: MainActivity
+    private lateinit var mactivity: MainActivity
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: DatabaseReference
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view=inflater.inflate(R.layout.users, container, false)
+
         mactivity=(activity as MainActivity)
+        auth=mactivity.getFirebase()
+        db=mactivity.getFirebaseDatabase()
+
         mactivity.supportActionBar?.title = "Users"
+
         val list = view.findViewById<ListView>(R.id.list)
-        val users = arrayListOf ("Steve","Alex","Bob")
-        list.adapter = ArrayAdapter<String>(
-            requireContext(),
-            android.R.layout.simple_list_item_1,
-            users
-        )
+
+        db.child("users").get().addOnSuccessListener {
+            val array = ArrayList<String>()
+            for(ds in it.children) {
+                array.add(ds.child("displayName").value.toString())
+            }
+            list.adapter = ArrayAdapter<String>(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                array
+            )
+        }
         return view
     }
 }
