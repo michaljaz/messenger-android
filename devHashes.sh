a3add124b1a0f3c4c61ab563c15924a7445d98d5
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-06-14 18:14:23 +0200

    new version

--- devHashes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "====================== FACEBOOK HASH ======================"
-keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore -storepass android | openssl sha1 -binary | openssl base64
-
-echo "====================== GOOGLE HASH ======================"
-keytool -keystore ~/.android/debug.keystore -list -v -storepass android
-
