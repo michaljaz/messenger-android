c5ca73aa104177cc0f4fdb701a1083e1dd1699a4
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-07-14 13:05:58 +0200

    remove debug hashes

--- debug_hashes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "====================== GOOGLE HASH ======================"
-keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore -storepass android | openssl sha1 -c
-
-echo "====================== FACEBOOK HASH ======================"
-keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore -storepass android | openssl sha1 -binary | openssl base64
